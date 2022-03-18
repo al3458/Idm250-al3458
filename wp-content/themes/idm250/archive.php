7cb17eb1704cb681f55c1d3eae42228929e06412
al3458 <71910268+al3458@users.noreply.github.com>
2022-03-17 22:12:57 -0400

    more

--- wp-content/themes/idm250/archive.php
+++ wp-content/themes/idm250/archive.php
@@ -1,4 +1,27 @@
-<?php get_header();?>
+<?php
 
+get_header(); ?>
 
-<?php get_footer();?>
+<h1 class="container"><?php the_archive_title(); ?>
+</h1>
+<div class="container sidebar">
+  <div class="sidebar-main grid-2">
+    <?php
+    while (have_posts()) : the_post();
+      get_template_part('components/project-teaser');
+    endwhile;
+    the_posts_pagination(
+        [
+            'mid_size' => 2,
+            'prev_text' => 'Previous',
+            'next_text' => 'Next'
+        ]
+    );
+  ?>
+  </div>
+  <aside class="sidebar-aside">
+    <?php dynamic_sidebar('sidebar-primary'); ?>
+  </aside>
+</div>
+
+<?php get_footer();
